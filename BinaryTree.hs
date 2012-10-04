03bb719a510d1420b89bf26971beecf155cd8bed
Nicolas Bock <nicolasbock@gmail.com>
2012-10-03 22:32:38 -0600

    Adding solution for problem 1.

--- BinaryTree.hs
+++ BinaryTree.hs
@@ -2,6 +2,7 @@ data BinaryTree a = Node a (BinaryTree a) (BinaryTree a)
                   | Empty
                   deriving (Show)
 
+-- Convert list into balanced but unsorted binary tree.
 fromList (x:xs) = Node x (fromList left) (fromList right)
                   where
                     temp  = splitAt (div (length xs) 2) xs
---
+++ Project Euler/Problem_1.hs
@@ -0,0 +1,5 @@
+problem_1 (x:xs)
+  | mod x 3 == 0 = x + problem_1 xs
+  | mod x 5 == 0 = x + problem_1 xs
+  | otherwise    = 0 + problem_1 xs
+problem_1 [] = 0
