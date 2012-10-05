556c43997b4e6d74549a7e96212698a6c8f4fb86
Nicolas Bock <nicolasbock@gmail.com>
2012-10-05 06:49:29 -0600

    Working on problem 2.

--- Project Euler/Problem_2.hs
+++ Project Euler/Problem_2.hs
@@ -4,4 +4,12 @@ fibonaci n
   | n < 1     = error "illegal value for n"
   | otherwise = (fibonaci (n-1)) + (fibonaci (n-2))
 
-problem_2 max = 
+doSum (fib_2, fib_1) (x:xs) = do
+    let fib = fib_2 + fib_1
+    let a   = if even fib then fib else 0
+    a + doSum (fib_1, fib) xs
+doSum (fib_2, fib_1) [] = 0
+
+findSum max
+  | max > 4   = doSum (1, 2) [2, 4 .. max]
+  | otherwise = error "max is too small"
