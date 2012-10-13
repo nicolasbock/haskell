4422957dd3ae4f8ab8f3271648ce731c7f03dd19
Nicolas Bock <nicolasbock@gmail.com>
2012-10-13 15:05:16 -0600

    Checkpoint.

--- Project Euler/Problem_3.hs
+++ Project Euler/Problem_3.hs
@@ -1,6 +1,9 @@
 -- Prime factorization.
 getFactor i lastF fs =
-  if mod i (lastF+2) == 0 then
-    getFactor (div i (lastF+2)) (lastF+2) ((lastF+2):fs)
+  if mod i lastF == 0 then
+    getFactor (div i lastF) lastF (lastF:fs)
   else
-    getFactor i (lastF+2) fs
+    if i <= lastF then fs
+    else getFactor i lastF fs
+  where
+    lastF = lastF+1
