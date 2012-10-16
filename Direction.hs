39f6a3164e0947fbd3cd846e41c39e5aa6806f5d
Nicolas Bock <nicolasbock@gmail.com>
2012-10-16 06:22:29 -0600

    Added direction list.

--- Direction.hs
+++ Direction.hs
@@ -1,4 +1,4 @@
-data Direction = LeftTurn | RightTurn | Straight
+data Direction = LeftTurn | RightTurn | Straight | UTurn
   deriving Show
 
 data Point = Point
@@ -9,23 +9,28 @@ data Point = Point
   deriving (Eq, Show)
 
 dot a b   = (x a)*(x b) + (y a)*(y b)
-cross a b = (x a)*(y b) - (y a)*(x a)
-minus a b = Point ((x a)-(x b)) ((y a)-(y b))
+cross a b = (x a)*(y b) - (y a)*(x b)
+minus a b = Point ((x a) - (x b)) ((y a) - (y b))
 
 getDirection a b c
   | cross leg1 leg2 > 0 = LeftTurn
   | cross leg1 leg2 < 0 = RightTurn
-  | otherwise           = Straight
+  | dot a b > 0         = Straight
+  | otherwise           = UTurn
   where
     leg1 = b `minus` a
     leg2 = c `minus` b
+
+getDirectionList (a:b:c:as) = [ getDirection a b c ] : (getDirectionList (b:c:as))
+getDirectionList (a:b:[])   = []
+
+a = Point  0  0
+b = Point  1  1
+c = Point  0  2
+d = Point (-1)  1
 
 main = do
     putStrLn (show (getDirection a b c))
     putStrLn (show (getDirection c b a))
     putStrLn (show (getDirection a b d))
-  where
-    a = Point 0   0
-    b = Point 0.5 1
-    c = Point 1   0
-    d = Point 1   2
+    putStrLn (show (getDirectionList [ a, b, c, d, a, d, c, b, a ]))
