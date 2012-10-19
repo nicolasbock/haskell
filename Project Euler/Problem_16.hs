93ad2afb58d291d8c3b7188850f6ecf441d147cc
Eric Chisolm <eric.chisolm@gmail.com>
2012-10-18 20:25:37 -0600

    Solution to Problem 16.

---
+++ Project Euler/Problem_16.hs
@@ -0,0 +1,7 @@
+digits n list
+  | n==0 && list==[] = [0]
+  | n==0             = list
+  | otherwise        = digits (n`div`10) ((n`mod`10):list)
+
+main = do
+  putStrLn (show (sum (digits (2^1000) [])))
