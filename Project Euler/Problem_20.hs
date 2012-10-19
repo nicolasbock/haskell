402a9c4e93d8f9540df2b39445321d5a439899e9
Eric Chisolm <eric.chisolm@gmail.com>
2012-10-18 22:27:12 -0600

    Changed code for solution to Problem 20.
    It's fun to define factorials recursively, but product [1..n] does the
    same thing, so why not use it?

--- Project Euler/Problem_20.hs
+++ Project Euler/Problem_20.hs
@@ -3,9 +3,5 @@ digits n list
   | n==0             = list
   | otherwise        = digits (n`div`10) ((n`mod`10):list)
 
-factorial n
-  | n == 0    = 1
-  | otherwise = n * factorial (n-1)
-
 main = do
-  putStrLn (show (sum (digits (factorial 100) [])))
+  putStrLn (show (sum (digits (product [1..100]) [])))
