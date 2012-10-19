ecbabe16c4d87878486ca68346851d0dcf22cbba
Eric Chisolm <eric.chisolm@gmail.com>
2012-10-18 21:07:51 -0600

    Changed solution to Problem 9.
    The first solution was right, but it's much easier to see that this one
    is right.  Also, I want it to work for generic values of the sum, but
    it didn't because the range on b was wrong; now it's right.

--- Project Euler/Problem_9.hs
+++ Project Euler/Problem_9.hs
@@ -1,4 +1,4 @@
-prod sum = [a*b*(sum-a-b) | b<-[1..998], a<-[1..b], 2*a*b - 2*(a+b)*sum + sum^2 == 0]
+prod sum = [a*b*(sum-a-b) | b<-[1..sum-2], a<-[1..b], a^2 + b^2 == (sum-a-b)^2]
 
 main = do
   putStrLn (show (prod 1000))
