0c0e93352d081324a02e976c1b9a5fc92e997a20
Nicolas Bock <nicolasbock@gmail.com>
2012-10-24 10:58:11 -0600

    Implemented sieve of Sundaram for problem 10.
    In it's current implementation the code is pretty slow.

--- Project Euler/nick/Problem_10.hs
+++ Project Euler/nick/Problem_10.hs
@@ -1,14 +1,13 @@
 import Data.List
 
-getList maxNum = [ i+j+2*i*j | i <- [ 1..(maxNum) ], j <- [ i..(maxNum) ], i+j+2*i*j <= (maxNum) ]
+sundaram maxN = 2:(shiftElements (doubleElements ([1..(div (maxN-2) 2)] \\ getList (div (maxN-2) 2))))
+  where
+    getList maxNum        = [ i+j+2*i*j | i <- [ 1..(maxNum) ], j <- [ i..(maxNum) ], i+j+2*i*j <= (maxNum) ]
+    doubleElements (n:ns) = ((2*n):(doubleElements ns))
+    doubleElements []     = []
+    shiftElements (n:ns)  = ((n+1):(shiftElements ns))
+    shiftElements []      = []
 
-doubleElements (n:ns) = ((2*n):(doubleElements ns))
-doubleElements []     = []
+sumSundaram maxN = sum ([ x | x <- (sundaram maxN), x < maxN ] )
 
-shiftElements (n:ns) = ((n+1):(shiftElements ns))
-shiftElements []     = []
-
-sundaram ns = 2:(shiftElements (doubleElements (ns \\ getList (maximum ns))))
-
-main = putStrLn ( show (sum ([ x | x <- (sundaram [ 1..(div maxN 2) ]), x < maxN ] )))
-  where maxN = 100000
+eratosthenes maxN = 0
