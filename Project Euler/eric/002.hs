232a2a24b73e5b995e2b7117f7f0411d21bad514
Eric Chisolm <eric.chisolm@gmail.com>
2012-10-28 16:29:52 -0600

    Simplified Problem 2 solution.

--- Project Euler/eric/002.hs
+++ Project Euler/eric/002.hs
@@ -2,9 +2,7 @@ fibonacci 1 = 1
 fibonacci 2 = 2
 fibonacci n = fibonacci (n-1) + fibonacci (n-2)
 
-fiblist max list
- | list /= [] && list !! 0 >= max = list
- | otherwise                      = fiblist max ((fibonacci ((length list)+1)):list)
+fiblist max = takeWhile (< max) (map fibonacci [1..])
 
 sumEven [] = 0
 sumEven (n:list)
@@ -12,4 +10,4 @@ sumEven (n:list)
  | even n     = n + sumEven list
  | odd  n     =     sumEven list
 
-main = putStrLn (show (sumEven (fiblist 4000000 [])))
+main = putStrLn . show . sumEven $ fiblist 4000000
