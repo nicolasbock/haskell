8f66782f57f097c12bf82b585a95aabdfd64c934
Eric Chisolm <eric.chisolm@gmail.com>
2012-10-28 09:15:17 -0600

    Fixed Problem 14 (still slow though).

--- Project Euler/eric/Problem_014.hs
+++ Project Euler/eric/Problem_014.hs
@@ -5,12 +5,14 @@ collatz n
  | even n = n:(collatz (n `div` 2))
  | odd n  = n:(collatz (3*n+1)) 
 
-maxLength n = maxLength' [2..n] (2,2)
+maxLength n = maxLength' [3..n] (2,2)
 
 maxLength' (n:list) (number, collatzLength)
- | list == []                = number
- | newLength > collatzLength = maxLength' list (n, newLength)
- | otherwise                 = maxLength' list (number, collatzLength)  
+ | list == [] = if test then n else number
+ | otherwise  = if test
+                then maxLength' list (n, newLength)
+                else maxLength' list (number, collatzLength)  
  where newLength = length (collatz n)
+       test      = newLength > collatzLength
 
 main = putStrLn (show (maxLength 999999 ))
