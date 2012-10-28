f8f89d631c945195f1e538fed70de0049d5bd406
Eric Chisolm <eric.chisolm@gmail.com>
2012-10-28 16:39:19 -0600

    Simplified Problem 2 even further with lots of dots and dollar signs.

--- Project Euler/eric/002.hs
+++ Project Euler/eric/002.hs
@@ -2,12 +2,6 @@ fibonacci 1 = 1
 fibonacci 2 = 2
 fibonacci n = fibonacci (n-1) + fibonacci (n-2)
 
-fiblist max = takeWhile (< max) (map fibonacci [1..])
+evenfibsum max = sum . takeWhile (<= max) . filter even $ map fibonacci [1..]
 
-sumEven [] = 0
-sumEven (n:list)
- | list == [] = 0
- | even n     = n + sumEven list
- | odd  n     =     sumEven list
-
-main = putStrLn . show . sumEven $ fiblist 4000000
+main = putStrLn . show $ evenfibsum 4000000
