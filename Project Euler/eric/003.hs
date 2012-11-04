9d412f0affe013b981071ab302cdff9709973a1f
Eric Chisolm <eric.chisolm@gmail.com>
2012-11-04 14:44:16 -0700

    My version of Nick's solution to Problem 3.

--- Project Euler/eric/003.hs
+++ Project Euler/eric/003.hs
@@ -1,16 +1,14 @@
---This is clear and correct but it will never finish running.
+primeFactors n 
+ | n <= 1    = []
+ | n <= 3    = [(n,1)]
+ | otherwise = allPrimeFactors 2 n
+  where allPrimeFactors k n
+         | k > n          = []
+         | n `mod` k == 0 = (k, snd $ remove k n):(allPrimeFactors (k+1) (fst $ remove k n))
+         | otherwise      = allPrimeFactors (k+1) n
+        remove k n = remove' k n 0
+          where remove' k n m
+                 | n `mod` k /= 0 = (n, m)
+                 | otherwise      = remove' k (n `div` k) (m+1) 
 
-factors n
- | n <= 0    = error "Not a counting number"
- | n == 1    = [1]
- | n == 2    = [1, 2]
- | otherwise = [1] ++ [m | m <- [2..n-1], n `mod` m == 0] ++ [n]
-
-isPrime m 
- | m == 1             = False
- | factors m == [1,m] = True 
- | otherwise          = False
-
-primeFactors n = [m | m <- factors n, isPrime m]
-
-main = putStrLn . show . maximum $ primeFactors 600851475143
+main = putStrLn . show . fst . last $ primeFactors 600851475143
